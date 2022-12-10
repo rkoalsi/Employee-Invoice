e164305aa12374bf664f9b89a986fc0b331ecf1e
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2022-12-10 12:22:17 +0530

    Replacing react with next js and adding minor backend configuration

---
+++ back.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd backend
+yarn start
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* GET users */
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
