b38371c445c2a881a274bff0a9b6d88037e8e6af
Pranav <pranav04092002@gmail.com>
2023-03-13 14:02:54 +0530

    Added Customer Buying Functionality

--- frontend/pages/product/[slug].tsx
+++ frontend/pages/product/[slug].tsx
@@ -1,7 +1,23 @@
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
 import { getProduct, getProducts } from '../../api/product';
-import { Box, Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import CloseIcon from '@mui/icons-material/Close';
+
 interface Data {
   _id: string;
   name: string;
@@ -18,10 +34,21 @@ interface Data {
 }
 function ProductPage(props: { data: Data; hasError: boolean }) {
   const router = useRouter();
+  const [age, setAge] = React.useState('');
+  const [val, setVal] = React.useState('');
 
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
   if (router.isFallback) {
     return <h1>Loading...</h1>;
   }
+
   return (
     <Box
       display={'flex'}
@@ -30,7 +57,7 @@ function ProductPage(props: { data: Data; hasError: boolean }) {
       justifyContent={'center'}
       flexDirection={'column'}
       gap={'8px'}
-    >
+     >
       <Typography variant='h2'>Product ID: {props.data._id}</Typography>
       <Typography variant='h2'>Product Name: {props.data.name}</Typography>
       <Typography variant='h2'>Product Sku: {props.data.sku} </Typography>
@@ -40,7 +67,90 @@ function ProductPage(props: { data: Data; hasError: boolean }) {
       <Typography variant='h2'>
         Product Stock: {props.data.stock} pcs
       </Typography>
+        <Box  display={'flex'}
+        mt={'20px'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        flexDirection={'row'}
+        gap={'16px'}>  
+    <Box sx={{ minWidth: 120 }}>
+      <TextField
+            id="outlined-number"
+            label="Quantity"
+            placeholder='eg: 1'
+            type="number"
+            InputProps={{
+              inputProps: { 
+                pattern: "[0-9]*",
+                min: 1 }
+            }}
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+          />
     </Box>
+
+    <Box sx={{ minWidth: 120 }}>
+      <Button 
+      variant="contained" 
+      color='success' 
+      size='large'
+      onClick={handleOpen}
+      startIcon={<InventoryIcon/>}>BUY</Button>
+      <Dialog open={open} onClose={handleClose}>
+      {/* <IconButton
+              //edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            ><CloseIcon />
+      </IconButton> */}
+        <DialogTitle align='center'>Details</DialogTitle>
+        <DialogContent>
+        <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="address"
+            label="Address"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="phone"
+            label="Phone Number"
+            type="tel"
+            fullWidth
+            variant="standard"
+          />
+        <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button startIcon={<CloseIcon/>} onClick={handleClose}>Close</Button>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  </Box>
+</Box>
   );
 }
 
